13ccec6f2397562bddb5ef638c02a7f8e3af8138
cherchen <sasashishi@qq.com>
2021-11-20 21:59:06 +0800

    ok

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,34 +1,24 @@
 // import { useState } from "react";
 
+import { useState,useEffect } from "react";
+
 export function Hello({ contract}) {
-//   const [checked, setChecked] = useState(done);
 
-    // const text =contract.say_hi({"arg_name":""},300000000000000);
+    const [text,setText] = useState([]);
 
-    // console.log('fuck ', text);
 
-    const x = () => {
-        contract.say_hi(
-            {"arg_name":""}
-        );
-    }
+    useEffect(() => {
+        // TODO: don't just fetch once; subscribe!
+        contract.say_hi({"arg_name":""}).then(text => {
+            setText(text)});
+      }, []);
 
-    // console.log('fuck ', x);
-//   const complete = ({ target }) => {
-//     setChecked(target.checked);
-//     contract.update({ id, updates: { task, done: target.checked } });
-//   };
-
-//   const del = () => {
-//     // on clicking the delete button invoke the del method on
-//     // the smart contract
-//     contract.del({ id });
-//   };
+      console.log(text);
 
   return (
     <>
       <p>
-        {x} 
+        {text} 
       </p>
     </>
   );
